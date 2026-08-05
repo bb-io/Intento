dd6333fe17b620fa20a70630ab97d65007f36cf2
Artem Riabushenko <ariabushenko@blackbird.io>
2026-08-05 19:27:37 +0300

    Added datasources for langueage and glossary ID

--- Apps.Intento/Model/Request/CreateOrUpdateGlossaryRequest.cs
+++ Apps.Intento/Model/Request/CreateOrUpdateGlossaryRequest.cs
@@ -1,6 +1,8 @@
 using Apps.Intento.DataHandlers.Static;
+using Apps.Intento.DataHandlers;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.Intento.Model.Request;
@@ -11,6 +13,7 @@ public class CreateOrUpdateGlossaryRequest
     public FileReference Glossary { get; set; } = default!;
 
     [Display("Glossary ID", Description = "Provide an existing glossary ID to update it; leave empty to create a new glossary")]
+    [DataSource(typeof(GlossaryDataHandler))]
     public string? GlossaryId { get; set; }
 
     [Display("Glossary name", Description = "Required when the TBX file does not contain a title; ignored when updating")]
@@ -21,8 +24,10 @@ public class CreateOrUpdateGlossaryRequest
     public string? Type { get; set; }
 
     [Display("Source language", Description = "Language code of source terms in the TBX file")]
+    [DataSource(typeof(LanguageDataHandler))]
     public string SourceLanguage { get; set; } = string.Empty;
 
     [Display("Target language", Description = "Language code of target terms in the TBX file")]
+    [DataSource(typeof(LanguageDataHandler))]
     public string TargetLanguage { get; set; } = string.Empty;
 }
--- Apps.Intento/Model/Request/DownloadGlossaryRequest.cs
+++ Apps.Intento/Model/Request/DownloadGlossaryRequest.cs
@@ -1,10 +1,13 @@
+using Apps.Intento.DataHandlers;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Intento.Model.Request;
 
 public class DownloadGlossaryRequest
 {
     [Display("Glossary ID")]
+    [DataSource(typeof(GlossaryDataHandler))]
     public string GlossaryId { get; set; } = string.Empty;
 
     [Display("Include draft terms", Description = "If true, download draft terms instead of only the currently published terms")]
